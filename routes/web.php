bfb8656840b4e5b6083fc12f29a69f923ff74534
eelybba <abbyoxide01@gmail.com>
2024-06-13 15:54:46 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ Route::middleware('role:cashier')->group(function () {
 });
 
 //Report Module
-// Only Admin and Coordinator can access this route
+// KAFA Admin , teacher and MUIP admin- change the role to ..
 Route::middleware('role:admin,coordinator')->group(function () {
     Route::get('/report', [ReportController::class, 'index'])->name('report');
     Route::post('/report', [ReportController::class, 'index'])->name('report');
@@ -104,3 +104,24 @@ Route::middleware('role:admin')->group(function () {
 
 // All user can access this route
 Route::get('/announcementList', [AnnouncementController::class, 'announcementList'])->name('announcementList');
+
+
+//student Result
+
+Route::middleware('role:admin')->group(function () {
+   
+    Route::get('/dashboard/announcements', [AnnouncementController::class, 'index'])->name('announcement');
+    Route::get('/announcements/add', [AnnouncementController::class, 'create'])->name('addAnnouncement');
+    Route::post('/announcements/store', [AnnouncementController::class, 'store'])->name('storeAnnouncement');
+    Route::get('/announcements/edit/{id}', [AnnouncementController::class, 'edit'])->name('editAnnouncement');
+    Route::post('/announcements/update/{id}', [AnnouncementController::class, 'update'])->name('updateAnnouncement');
+    Route::post('/announcements/delete/{id}', [AnnouncementController::class, 'destroy'])->name('deleteAnnouncement');
+
+//Report Module
+// KAFA Admin , teacher and MUIP admin- change the role to ..
+Route::middleware('role:admin,coordinator')->group(function () {
+    Route::get('/report', [ReportController::class, 'index'])->name('report');
+    Route::post('/report', [ReportController::class, 'index'])->name('report');
+    Route::get('report/data/{range}', [ReportController::class, 'getData'])->name('report.data');
+    Route::get('/report/export', [ReportController::class, 'exportCSV'])->name('csv');
+});
