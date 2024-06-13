239891351751ba5fe22ed47804ae24f2336c27a1
JunTa1012 <junta101219@gmail.com>
2024-06-13 18:22:38 +0800

    add

--- resources/views/ManageKafaActivity/addKafaActivityForm.blade.php
+++ resources/views/ManageKafaActivity/addKafaActivityForm.blade.php
@@ -11,7 +11,7 @@
                     <form action="{{ route('storeKafaActivity') }}" method="POST">
                         @csrf
                         <div class="mb-4">
-                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name:</label>
+                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Activity Name:</label>
                             <input type="text" name="activity_name" id="title" class="form-input rounded-md shadow-sm mt-1 block w-full"
                                    required autofocus/>
                             @error('activity name')
@@ -19,7 +19,7 @@
                             @enderror
                         </div>
                         <div class="mb-4">
-                            <label for="desc" class="block text-gray-700 text-sm font-bold mb-2">Description:</label>
+                            <label for="desc" class="block text-gray-700 text-sm font-bold mb-2">Activity Description:</label>
                             <textarea name="activity_desc" id="activity_desc" class="form-textarea rounded-md shadow-sm mt-1 block w-full"
                                       rows="4" required></textarea>
                             @error('activity desc')
@@ -27,7 +27,7 @@
                             @enderror
                         </div>
                         <div class="mb-4">
-                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Date and Time:</label>
+                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Activity Datetime:</label>
                             <input type="datetime-local" name="activity_dateTime" id="activity_dateTime" class="form-input rounded-md shadow-sm mt-1 block w-full"
                                     required autofocus/>
                             @error('activity_dateTime')
@@ -35,7 +35,7 @@
                             @enderror
                         </div>
                         <div class="mb-4">
-                            <label for="age" class="block text-gray-700 text-sm font-bold mb-2">Age:</label>
+                            <label for="age" class="block text-gray-700 text-sm font-bold mb-2">Activity Age for Student:</label>
                             <input type="number" name="activity_studentAge" id="age" class="form-input rounded-md shadow-sm mt-1 block w-full"
                                    value="{{ old('age') }}" required autofocus min="1" max="120"/>
                             @error('age')
@@ -43,21 +43,26 @@
                             @enderror
                         </div>                        
                         <div class="mb-4">
-                            <label for="comment" class="block text-gray-700 text-sm font-bold mb-2">Comment:</label>
+                            <label for="comment" class="block text-gray-700 text-sm font-bold mb-2">Activity Comment:</label>
                             <textarea name="activity_comment" id="comment" class="form-textarea rounded-md shadow-sm mt-1 block w-full"
                                       rows="4" required></textarea>
                             @error('comment')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
                         </div>
-                        <div class="flex justify-end px-4 py-2">
+                        <div class="flex justify-center px-4 py-2">
                             <div class="px-4">
-                                <input type="reset" value="Reset" class="btn border border-slate-400 bg-gray-400 px-3 py-2 rounded-xl hover:bg-gray-300">
-                            </div>
+                                <input type="button" value="Cancel" class="btn border border-slate-400 bg-red-400 px-3 py-2 rounded-xl hover:bg-gray-300 w-full" onclick="handleCancel()">                            </div>
                             <div class="px-4">
-                                <input type="submit" value="Save" class="btn btn-success border border-slate-300 bg-emerald-500/80 px-3 py-2 rounded-xl hover:bg-emerald-400/80">
+                                <input type="submit" value="  Save  " class="btn btn-success border border-slate-400 bg-emerald-500/80 px-3 py-2 rounded-xl hover:bg-emerald-400/80">
                             </div>
                         </div>
+                        <script>
+                            function handleCancel() {
+                                // Logic to clear form fields or perform other cancel actions
+                                window.location.href = "{{ route('kafaActivity') }}"; // Example: reset form fields
+                            }
+                        </script>
                     </form>
                 </div>
             </div>
