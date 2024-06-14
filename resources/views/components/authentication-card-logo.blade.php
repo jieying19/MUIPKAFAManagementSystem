5bcec5d8d1875cb66d4968df93824ef6f170727a
Wan Ashraf <wanashraf2511@gmail.com>
2024-06-14 08:17:56 +0800

    Kafa and Muip logo for login

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,9 @@
-<a href="/">
-    <img src="{{ asset('images/kafa-logo.png') }}" alt="PMMS" style="height: 10rem"/>
+<a>
+<div class="flex flex-col items-center justify-center">
+    <div class="flex items-center space-x-4">
+        <img src="{{ asset('images/logo-muip.png') }}" alt="MKFMS" style="height: 10rem"/>
+        <img src="{{ asset('images/kafa-logo.png') }}" alt="MKFMS" style="height: 10rem"/>
+    </div>
+    <h1 class="text-center text-xl mt-4">MUIP KAFA Management System (MKFMS)</h1>
+</div>
 </a>
