ede5bf4b1b688481cba21a09084c66f4cecca8e0
Wan Ashraf <wanashraf2511@gmail.com>
2024-06-14 09:57:06 +0800

    removed report and duty roster because conflict

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -113,40 +113,9 @@
 
         {{-- Report --}}
         {{-- Only KAFA Admin and MUIP Admin can access this module --}}
-        @if (Auth::user()->role == 'KAFAadmin' || Auth::user()->role == 'MUIPadmin'|| Auth::user()->role == 'teacher')
-        <a href="{{ route('report') }}">
-            <x-nav-item>
-                <div class="self-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                    </svg>
-                </div>
-                <div>
-                    REPORT
-                </div>
-            </x-nav-item>
-        </a>
-        @endif
 
         {{-- Student result management --}}
         {{-- Only KAFAadmin can access this module --}}
-        @if (Auth::user()->role == 'KAFAadmin')
-        <a href="{{ route('DutyRoster') }}">
-            <x-nav-item>
-                <div class="self-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                    </svg>
-                </div>
-                <div>
-                    DUTY ROSTER
-                </div>
-            </x-nav-item>
-        </a>
-        @endif
+
     </ul>
 </nav>
